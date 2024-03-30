96e6e31beabd5c0843ffa3bc72ccdc6d1042cbec
stranik28 <jb357506@gmail.com>
2024-03-30 04:26:23 +0300

    first look

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,99 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type Metrics struct {
+	gauge   float64
+	counter int64
+}
+
+type MemStorage struct {
+	metrics map[string]Metrics
+}
+
+func NewMemStorage() *MemStorage {
+	return &MemStorage{
+		metrics: make(map[string]Metrics),
+	}
+}
+
+func (mem *MemStorage) setMemStorage(key string, value Metrics) {
+	mem.metrics[key] = value
+}
+
+func (mem *MemStorage) getMemStorage(key string) (Metrics, bool) {
+	value, err := mem.metrics[key]
+	return value, err
+}
+
+func updateMetrics(res http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		http.Error(res, "METHOD NOT ALLOWED", 405)
+		return
+	}
+	url := req.URL.Path
+	params := strings.Split(url, "/")
+	params = params[1:]
+	if len(params) != 4 {
+		http.Error(res, "Недопустимое количество параметров", http.StatusNotFound)
+		return
+	}
+	switch params[1] {
+	case "counter":
+		value, err := strconv.ParseInt(strings.TrimSpace(params[3]), 10, 64)
+		if err != nil {
+			http.Error(res, "Значение должно быть в формате int64", 422)
+			return
+		}
+		val, ok := storage.getMemStorage(params[2])
+		if !ok {
+			val = Metrics{
+				gauge:   0,
+				counter: value,
+			}
+		} else {
+			val.counter += value
+		}
+		storage.setMemStorage(params[2], val)
+	case "gauge":
+		value, err := strconv.ParseFloat(strings.TrimSpace(params[3]), 64)
+		if err != nil {
+			http.Error(res, "Значение должно быть в формате float64", 422)
+			return
+		}
+		val, ok := storage.getMemStorage(params[2])
+		if !ok {
+			val = Metrics{
+				gauge:   value,
+				counter: 0,
+			}
+		} else {
+			val.gauge = value
+		}
+		storage.setMemStorage(params[2], val)
+	default:
+		http.Error(res, "Недопустимый тип метрики", 422)
+		return
+	}
+
+	res.WriteHeader(http.StatusOK)
+	return
+}
+
+var storage = NewMemStorage()
+
+func main() {
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/update/", updateMetrics)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
